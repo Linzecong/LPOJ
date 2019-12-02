e6d3a130be3b12dff138ec12e44dc28077c66e05
280051755@qq.com <280051755@qq.com>
2019-12-02 22:01:45 +0800

    更新

--- CrawlingServer/CodeforcesRate.py
+++ CrawlingServer/CodeforcesRate.py
@@ -9,9 +9,9 @@ def get_CF_Rate(name):
         
 #         print(response_data)
 #         response_data = re.sub('<span class="legendary-user-first-letter">','',response_data);
-        score = re.findall('<span style="font-weight:bold;" class=".*">[0-9]{2,4}</span>', response_data)[0]
+        score = re.findall('<span style="font-weight:bold;" class=".*?">[0-9]{2,4}</span>', response_data)[0]
 #         print(score)
-        score = re.findall('[0-9]{2,4}',score)[0]
+        # score = re.findall('[0-9]{2,4}',score)[0]
         return re.sub('[^0-9]','',score)
     except:
         return -1
